5239f94132d6de92801471e750788650cf8ca4f7
arwehrman <arwehrman@gmail.com>
2018-07-21 18:36:13 -0400

    updated reducer and action

--- src/components/DrillCard.js
+++ src/components/DrillCard.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-
 const DrillCard = ({ drill }) => {
 return (
       <div className="drillcard" >
         <h3>{drill.title}</h3>
-        <p>{drill.id}</p>
         <p>Description: {drill.description}</p>
         <p>Category: {drill.category}</p>
         <p>Likes: {drill.likes}</p>

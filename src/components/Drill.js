ef6d47bd0c0c76ed2fc256e31f02814dc7b9677f
arwehrman <arwehrman@gmail.com>
2018-08-25 12:38:01 -0400

    cleaning up README file

--- src/components/Drill.js
+++ src/components/Drill.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 
-const Drill = ({drill}) => (
-  <div>
-    <br />
-    <div className="drillcard">
-      <Link to={`/drills/${drill.id}`}>{drill.title}</Link>
-      <p>Category: {drill.category}</p>
-      <p>Skill Level: {drill.level}</p>
-    </div>
-  </div>
-)
+class Drill extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { like: 0 }
+  }
+
+
+  handleLike = () => {
+    console.log(this.state)
+    this.setState({
+      like: this.state.like + 1
+    })
+  }
+
+
+  render(){
+    const { drill } = this.props;
+    return (
+      <div>
+        <br />
+        <div className="drillcard">
+          <Link to={`/drills/${drill.id}`}>{drill.title}</Link>
+          <p>Category: {drill.category}</p>
+          <p>Skill Level: {drill.level}</p>
+          <p>Like: {this.state.like} </p>
+          <button onClick={this.handleLike}>Like</button>
+        </div>
+      </div>
+    )
+  }
+
+
+}
+
 
 
 export default Drill;

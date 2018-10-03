0028df3bfa1c106dfcc198e76f7c3d1f2198e857
arwehrman <arwehrman@gmail.com>
2018-10-02 21:03:12 -0400

    add cone image and css for image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import logo from '../images/cone2.png';
 
 const Header = () => (
   <div className="header">
+    <img src={logo} alt="cone"/>
     <h1>Drill It Kill It</h1>
   </div>
 );

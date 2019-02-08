19dadb751b2ab088312cfb10b26b8e3f9354b542
Amanda Wehrman <arwehrman@gmail.com>
2019-02-07 19:58:39 -0500

    changed Home page CSS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Contact.css'
 
 export const Contact = () => (
-  <div>
+  <div className="contact">
     <h2>Contact Us</h2>
     <p>Have suggestions to improve Drill It Kill it?</p>
     <p>please email us at: drillitkillit@gmail.com</p>

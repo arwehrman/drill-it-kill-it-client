5edb560b2c2929e30ed36f02dad8cae628dc0ee8
arwehrman <arwehrman@gmail.com>
2019-07-01 18:42:58 -0400

    added CSS and some content to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,21 +6,26 @@ const Home = () => (
   <div className="home">
 
     <h2>Welcome to Drill It Kill It</h2>
-    <br />
-    <p>Drill It Kill It is a central hub to keep track of and share roller derby drills.</p>
-    <br />
-    <br />
+   
+    <p>Drill It Kill It is a central hub to keep track of and share roller derby drills. Currently this a DEMO ONLY APP. I would like to go live with this app once all the necessary features are in place for a solid user experience.</p>
+    <div className="grid-container">
     <div className="homesection">
       <h3>Updates</h3>
-      <p>Section for Updates to site. </p>
+      <p>Future Plans for this App include:</p>
+      <ul>
+        <li>Adding User model</li>
+        <li>Likes feature fullly working</li>
+        <li>Photos and videos upload feature</li>
+      </ul>
     </div>
-    <br />
-    <br />
+    
     <div className="homesection">
       <h3>News</h3>
       <p>Latest in Derby news & drills</p>
     </div>
-    <br />
+
+    </div>
+   
   </div>
 );
 

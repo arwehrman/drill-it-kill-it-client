2dcd6e544e9222e6da11f55bc95f2e878c90630c
arwehrman <arwehrman@gmail.com>
2019-06-30 21:43:22 -0400

    added more rules to redirects

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NavBar = (props) => (
   <div  className="navigation">
     <Link to="/">Home</Link>
-    <Link to="/api/drills">Drills</Link>
+    <Link to="/drills">Drills</Link>
     <Link to="/contact">Contact</Link>
   </div>
 );
